16a79851063902d03d7b2e3afcce18ace11233c0
thisWeb1225 <shan881225@gmail.com>
2023-09-02 20:10:17 +0800

    ✨ Database: add schema.prisma with mongoDB

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,10 @@ const config: Config = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")({
+      stategy: 'class'
+    })
+  ],
 }
 export default config
