20049bd9fdd3d513e70f316efeeb31f87fcbdc2a
thisWeb1225 <shan881225@gmail.com>
2024-01-31 15:45:15 +0800

    🐛 Fix path problem

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,4 +1,4 @@
-import { authOptions } from '../api/auth/[...nextauth]/route';
+import { authOptions } from "../libs/authOptions";
 import { getServerSession } from 'next-auth';
 
 export default async function getSession() {
